f238a0c80634cffab1afb75268bd8c804ff2a016
TomPickett <Pickett.tom7@gmail.com>
2015-02-11 19:36:48 -0500

    created createjs site structure

---
+++ COMP2068-CreatejsDemo/Scripts/game.js
@@ -0,0 +1,21 @@
+﻿var canvas;
+var stage;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+
+function gameLoop() {
+    stage.update();
+}
+
+function main() {
+    var helloText = new createjs.Text("Hello World", "40 px consolas", "#000000");
+
+    stage.addChild(helloText);
+}
+//# sourceMappingURL=game.js.map
